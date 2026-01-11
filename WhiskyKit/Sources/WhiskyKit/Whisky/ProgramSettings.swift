9abddc4e7add1f78acea7842c55e1934cc70596b
Adam Franke <afranke@gmail.com>
2026-01-11 04:14:29 -0500

    Fix: Apply swiftlint rule to single line

--- WhiskyKit/Sources/WhiskyKit/Whisky/ProgramSettings.swift
+++ WhiskyKit/Sources/WhiskyKit/Whisky/ProgramSettings.swift
@@ -75,7 +75,8 @@ public enum Locales: String, Codable, CaseIterable, Sendable {
     /// For example, Japanese returns "日本語" rather than "Japanese".
     ///
     /// - Returns: The localized display name for this locale.
-    public func pretty() -> String { // swiftlint:disable:this cyclomatic_complexity
+    // swiftlint:disable:next cyclomatic_complexity
+    public func pretty() -> String {
         switch self {
         case .auto:
             return String(localized: "locale.auto")
